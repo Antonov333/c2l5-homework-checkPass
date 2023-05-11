2af6a5974ad01a2dea71705759560eed6c9da0a7
Antonov333 <antonovsergei333@yahoo.com>
2023-05-11 12:04:56 +0300

    Correct class naming CheckPasswordController

--- src/main/java/pro/sky/course2lesson5/checkpassword/CheckPasswordController.java
+++ src/main/java/pro/sky/course2lesson5/checkpassword/CheckPasswordController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class checkPasswordController {
+public class CheckPasswordController {
 
     private CheckPasswordService checkPasswordService = new CheckPasswordService();
 
