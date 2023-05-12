b3e6c41f9cdfb76f750dddb4f8910a14a1f74ca5
Antonov333 <antonovsergei333@yahoo.com>
2023-05-12 22:09:15 +0300

    Coach notes fulfilled

--- src/main/java/pro/sky/course2lesson5/checkpassword/CheckPasswordApplication.java
+++ src/main/java/pro/sky/course2lesson5/checkpassword/CheckPasswordApplication.java
@@ -1,13 +1,21 @@
 package pro.sky.course2lesson5.checkpassword;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class CheckPasswordApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(CheckPasswordApplication.class, args);
+        System.out.println("\nCourse 2, Lesson 5 homework\n");
+        String login = "John",
+                password = "D_1hWiKjjP_9",
+                confirmPassword = "D_1hWiKjjP_9";
+        System.out.println("We are calling checkPassword method to check LOGIN: " + login + " PASSWORD: " + password +
+                " and CONFIRM PASSWORD: " + confirmPassword);
+        try {
+            CheckPasswordService.checkPassword(login, password, confirmPassword);
+        } catch (WrongLoginException wrongLoginException) {
+            System.out.println("... " + wrongLoginException);
+        } catch (WrongPasswordException wrongPasswordException) {
+            System.out.println("... " + wrongPasswordException);
+        } finally {
+            System.out.println("... checkPassword method completed");
+        }
     }
-
 }
